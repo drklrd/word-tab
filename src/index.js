2f8f1de9d2f71f8c37b676c09c6e22f3792d116d
drklrd <srvbhattarai@gmail.com>
2017-12-02 21:27:01 +0545

    add backgraound image and word json

--- src/index.js
+++ src/index.js
@@ -1,16 +1,32 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Words from './words.json';
 
+let randomWord = Words.words[Math.floor(Math.random() * Words.words.length)];
 
 class App extends React.Component{
 
 	render(){
 		return(
-			<div className="row">
-				<h1>eHELLO</h1>
+			<div className="container row app-background">
+				<img src="img/coffee.jpg" alt="Img" />
+				<div className="word">
+					<div className="title">
+                        {randomWord.word}
+					</div>
+					<div className="meaning">
+                        meaning &#xb7; {randomWord.meaning}
+					</div>
+					<div className="sentence">
+                        usage &#xb7; {randomWord.sentence}
+					</div>
+				</div>
 			</div>
 		)
 	}
 }
 
 ReactDOM.render(<App/>,document.getElementById("app"));
+
+
+// http://wallpaperswide.com/coffee_8-wallpapers.html
