e6356fd6910d83b0d3a0343c2e82cfa0ffec2302
drklrd <srvbhattarai@gmail.com>
2017-12-03 20:24:55 +0545

    add variying background

--- src/index.js
+++ src/index.js
@@ -2,15 +2,43 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Words from './words.json';
 
-let randomordIndex =Math.floor(Math.random() * Words.words.length);
+let randomordIndex = Math.floor(Math.random() * Words.words.length);
 let randomWord = Words.words[randomordIndex];
+let isLastWordActive = false;
+
+let images = ['coffee','balloon','book'];
+
+function updateLastWordStorage(){
+    randomordIndex =Math.floor(Math.random() * Words.words.length);
+    randomWord = Words.words[randomordIndex];
+    chrome.storage.local.set({
+        'lastword': Date.now()+'_'+randomordIndex
+    });
+}
+
+chrome.storage.local.get('lastword', (result) => {
+    if(!result.lastword){
+        updateLastWordStorage();
+    }else{
+    	if(Date.now()-result.lastword.split('_')[0] > (5 * 60 * 1000)){
+            updateLastWordStorage();
+    	}else{
+    		if(result.lastword.split('_')[2]){
+    			isLastWordActive = true;
+    		}
+    		randomWord = Words.words[result.lastword.split('_')[1]];	
+    	}
+    }
+    ReactDOM.render(<App/>,document.getElementById("app"));
+});
 
 class App extends React.Component{
 
 	constructor(props){
 		super(props);
 		this.state = {
-			hintVisible : false
+			hintVisible : false,
+            bgImg : images[Math.floor(Math.random()*images.length)]
 		};
 		this.showHint = this.showHint.bind(this);
 		this.addToFavorite = this.addToFavorite.bind(this);
@@ -23,6 +51,13 @@ class App extends React.Component{
 	}
 
 	showHint(){
+		chrome.storage.local.get('lastword', (result) => {
+		    if(result.lastword){
+		    	chrome.storage.local.set({
+		    	    'lastword': result.lastword+'_opened'
+		    	});
+		    }
+		});
 		this.setState({
 			hintVisible: true
 		});
@@ -64,7 +99,7 @@ class App extends React.Component{
 
 		return(
 			<div className="container row app-background">
-				<img src="img/coffee.jpg" alt="Img" />
+				<img  src={"img/"+this.state.bgImg+".jpg"} alt="Img" />
 				<div className="word">
 					<div className="title">
                         {randomWord.word}
@@ -72,10 +107,10 @@ class App extends React.Component{
 
 					</div>
 					{
-						this.state.hintVisible && hintTemplate
+						(this.state.hintVisible || isLastWordActive) && hintTemplate
 					}
 					{
-						!this.state.hintVisible &&
+						!this.state.hintVisible && !isLastWordActive &&
 						<div>
 							<button onClick={this.showHint} className="show-meaning">Show meaning</button>
 						</div>
